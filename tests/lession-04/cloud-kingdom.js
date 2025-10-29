1339d6798d51cdc3a3f2c00abd2264d76bfdc51c
tramnguyen <quynhtram.tgdd.tn@gmail.com>
2025-10-29 10:37:16 +0700

    feat: add solution for challenge 03

---
+++ tests/lession-04/cloud-kingdom.js
@@ -0,0 +1,9 @@
+let currentLives = 3;
+const level1 = 25;
+const level2 = 30;
+const level3 = 45;
+let average = (level1 + level2 + level3) / 3;
+console.log(average);
+let total = level1 + level2 + level3;
+let remainder = total % 3;
+console.log(remainder);
